d67f40d7414a83fb383c54622ea05ddea618ee52
Spencer Felton <sef210@exeter.ac.uk>
2020-05-20 01:28:16 +0100

    Upgrades
    Upgrade shop implemented
    1st point upgrade implemented

--- Game.js
+++ Game.js
@@ -1,53 +1,83 @@
 var generatorArray =[0,0,0,0,0,0,0,0,0,0]; // Start with 0 of each initial generator
 var generatorBaseCostArray =[1,1e2,1e4,1e6,1e8,1e10,1e12,1e14,1e16,1e18];
+var generatorCurrentCostArray =[1,1e2,1e4,1e6,1e8,1e10,1e12,1e14,1e16,1e18];
+var generatorMultipliers = [1,1,1,1,1,1,1,1,1,1]
 var playerCurrency = 1
 
-function determineGeneratorCost(i) {
-  var cost = 2.1**generatorArray[i]*generatorBaseCostArray[i];
-  return cost;
+function nextGeneratorCost(i) {
+  nextCost = 1+generatorCurrentCostArray[i]*2.1;
+  return nextCost;
 }
 
 function buyGenerator(i){ //Try to buy generator according to resourcesArray
-  var generatorCost = determineGeneratorCost(i) // cost of generator
+  var generatorCost = generatorCurrentCostArray[i] // cost of generator
   if (playerCurrency>=generatorCost) {
     generatorArray[i]+=1;
     playerCurrency-=generatorCost;
-    $("#point").text("Points: " + generatorArray[i]); // Update how many point generators player has
-
-    var newGeneratorCost = determineGeneratorCost(i); // calculate cost of next generator
-    $("#buyPointButton").text("Cost: " + newGeneratorCost.toFixed(0)); // update text value of button
-
-    var currencyPerSecond = currencyGainPerUpdate(0)*60; // currency gain per update is determined by points (0th index), *60 to get per second
-    $("#currencyPS").text("Cardinals/Sec: " + currencyPerSecond.toFixed(2));
+    generatorCurrentCostArray[i] = nextGeneratorCost(i);
   }
   else{
     console.log("you can't afford that!");
   }
 }
 
-function currencyGainPerUpdate(i){ // game runs at 60 updates per second, so to get gain per update we divide by 60
-  var gain = (generatorArray[i])/60
+function currencyGainPerUpdate(){ // game runs at 60 updates per second, so to get gain per update we divide point gains by 60 and any upgrades
+  var gain = (generatorArray[0]*generatorMultipliers[0])/60;
+  return gain;
+}
+
+function pointGainPerUpdate(){
+  var gain = (generatorArray[1])/60;
   return gain;
 }
 
 function updateCurrency(){ // points generate currency - calculate and update html every update
-  var gain = currencyGainPerUpdate(0);
+  var gain = currencyGainPerUpdate();
   playerCurrency += gain;
-  $("#CardinalCurrency").text("Cardinals: " + playerCurrency.toFixed(3)); //update the html with the amount of currency the player has
 }
 
 function updatePoints(){
-  var gain = currencyGainPerUpdate(0);
+  var gain = pointGainPerUpdate();
+  console.log(gain);
+  generatorArray[0] += gain;
+}
 
+function checkUpgrades(){
+  if (generatorArray[0] >= 5) {
+    $("#pointUpgrade1").show();
+  }
+}
+
+function updateDisplay(){
+  $("#CardinalCurrency").text("Cardinals: " + playerCurrency.toFixed(2));
+  $("#point").text("Points: " + generatorArray[0].toFixed(0));
+  $("#line").text("Lines: " + generatorArray[1].toFixed(0));
+
+  $("#buyPointButton").text("Cost: " + generatorCurrentCostArray[0].toFixed(0));
+  $("#buyLineButton").text("Cost: " + generatorCurrentCostArray[1].toFixed(0));
+
+  $("#currencyPS").text("Cardinals/Sec: " + (currencyGainPerUpdate()*60).toFixed(2));
+  $("#pointsPS").text("Points/Sec: " + (pointGainPerUpdate()*60).toFixed(2));
+
+  if(generatorArray[0] >= 5) {
+    $("#upgradeShopWrapper").show();
+  }
+  if (generatorArray[0] >= 10) { // if the player has 5 Points, show the next generator
+    $("#lineWrapper").show();
+  }
+  checkUpgrades();
 }
 
 function update(){ // update all aspects of the game
   updateCurrency();
-  if (generatorArray[0] >= 5) { // if the player has 5 Points, show the next generator
-    $("#lineWrapper").show();
-  }
+  updatePoints();
+
 }
+
 setInterval(loop, 16)
 function loop(){
   update();
+  updateDisplay();
+
+
 }
--- test.js
+++ test.js
@@ -5,4 +5,29 @@ $(document).ready(function(){
     console.log("you clicked!");
   });
 
+  $("#buyLineButton").click(function(){
+    buyGenerator(1);
+    console.log("you clicked!");
+  });
+
+  $("#homeButton").click(function(){ // clicking the home button hides other windows and shows the home window
+    $("#mainGameWindow").show();
+    $("#upgradeWindow").hide();
+  });
+
+  $("#upgradeShopButton").click(function(){
+    $("#mainGameWindow").hide();
+    $("#upgradeWindow").show();
+  });
+
+  $("#pointUpgrade1Button").click(function(){
+    if (playerCurrency >= 50) { // check the player has enough money to purchase the upgrade
+      playerCurrency -= 50;
+      generatorMultipliers[0] *= 2; // multiply currency generation by points by 2
+      $("#pointUpgrade1").hide(); // hide the button and description so it cant be pressed twice
+    }
+    else{
+      console.log("you cant afford that yet");
+    }
+  })
 });
