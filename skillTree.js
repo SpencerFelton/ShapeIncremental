fff05fe8a530f4a119fb71038acc721a600c196f
Spencer Felton <SpencerEFelton@gmail.com>
2020-07-07 21:44:55 +0100

    Upgrade cost readjustment
    Changed upgrade cost of generator 1 upgrades

--- skillTree.js
+++ skillTree.js
@@ -1,15 +1,15 @@
 var pointTree = {id:"pointUpgrade1", name:"node1", parent:{}, cost: 50, desc: "Point upgrade 1: +1 to the base output of Points", child:{
-  id:"pointUpgrade2", name:"node2", parent:"node1", cost: 50, desc:"Increases the output of points by another 1.5x", child:{
-    id:"pointUpgrade3", name:"node3", parent:"node2", cost: 50, desc:"Increases the output of points by 3x", child:[{
-      id:"pointUpgrade4", name:"node4", parent:"node3", cost: 50, desc:"Increases the output of points by 0.1x per Point bought", child:{
-        id:"pointUpgrade5", name:"node5", parent:"node4", cost: 50, desc:"Increases the output of points by", child:{
-          id:"pointUpgrade6", name:"node6", parent:"node5", cost: 50, desc:"Increases the output of points by", child:{}
+  id:"pointUpgrade2", name:"node2", parent:"node1", cost: 500, desc:"Increases the output of points by another 1.5x", child:{
+    id:"pointUpgrade3", name:"node3", parent:"node2", cost: 5000, desc:"Increases the output of points by 3x", child:[{
+      id:"pointUpgrade4", name:"node4", parent:"node3", cost: 50000, desc:"Increases the output of points by 0.1x per Point bought", child:{
+        id:"pointUpgrade5", name:"node5", parent:"node4", cost: 500000, desc:"Increases the output of points by", child:{
+          id:"pointUpgrade6", name:"node6", parent:"node5", cost: 5000000, desc:"Increases the output of points by", child:{}
         }
       }
     },{
-      id:"pointUpgrade7", name:"node7", parent:"node3", cost: 50, desc:"Increases the output of points by", child:{
-        id:"pointUpgrade8", name:"node8", parent:"node7", cost: 50, desc:"Increases the output of points by", child:{
-          id:"pointUpgrade9", name:"node9", parent:"node8", cost: 50, desc:"Increases the output of points by", child:{}
+      id:"pointUpgrade7", name:"node7", parent:"node3", cost: 50000, desc:"Increases the output of points by", child:{
+        id:"pointUpgrade8", name:"node8", parent:"node7", cost: 500000, desc:"Increases the output of points by", child:{
+          id:"pointUpgrade9", name:"node9", parent:"node8", cost: 5000000, desc:"Increases the output of points by", child:{}
         }
       }
     }]
